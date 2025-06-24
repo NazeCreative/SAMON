3587b13a67f2bb7e337536f68e65e0c9fe63eece
Tran Vo Ngoc Truong <119861415+NgTruong624@users.noreply.github.com>
2025-06-24 14:35:35 +0700

    feat : add FireBase

--- project_s/lib/main.dart
+++ project_s/lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import 'presentation/pages/welcome_page.dart';
 import 'presentation/pages/login_page.dart';
 import 'presentation/pages/signup_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
